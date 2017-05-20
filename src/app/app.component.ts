583101cf60f1f57ce4284d493b5f39f84d47f1e7
ShayDev <shay.nadav@gmail.com>
2017-05-20 17:36:59 +0300

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,17 @@
 import { Component } from '@angular/core';
+import { AbstractControl ,FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+export class User {
+  public first: string;
+  public last: string;
+  public user: string;
+  public pswd: string;
+  public confirm: string
+}
+
+function pswdMatcher(c: AbstractControl) {
+  return c.get('pswd').value === c.get('confirm').value ? null : { 'nomatch' : true }
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +19,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  private reactiveUser: User;
+
+  private templateFirstName: string;
+
+  // private myForm = new FormGroup({
+  // first: new FormControl(),
+  // last: new FormControl(),
+  // user: new FormControl(),
+  // pswd: new FormControl(),
+  // confirm: new FormControl()
+  // });
+
+  private myForm: FormGroup;
+
+
+  constructor(private fb: FormBuilder) {
+    this.templateFirstName ='tshay';
+
+    this.myForm = this.fb.group({
+      first: ['', [Validators.required]],
+      last: '',
+      account: this.fb.group({
+        user: '',
+        pswd: [, [Validators.required]],
+        confirm: [, [Validators.required]]
+      }, { validator: pswdMatcher})
+    });
+
+    this.myForm.patchValue({
+      first: 'shay',
+      last: 'nadav'
+    });
+  }
+
+  private onSubmit(form: FormGroup) {
+    console.log(form);
+    this.reactiveUser = form.value;
+    console.log(this.reactiveUser);
+  }
+
 }
